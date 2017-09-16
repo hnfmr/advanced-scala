25df558c7ca3e0edb56955b46ddc370f0b30daa4
Wei Qi <qiwei.uk@gmail.com>
2017-09-16 18:30:43 +1000

    traverse updates

--- src/main/scala/io/hnfmr/chapter7/TraverseEx.scala
+++ src/main/scala/io/hnfmr/chapter7/TraverseEx.scala
@@ -5,6 +5,12 @@ import scala.concurrent.Await
 import scala.concurrent.duration._
 import scala.concurrent.ExecutionContext.Implicits.global
 
+import scala.language.higherKinds
+import cats.Applicative
+import cats.syntax.applicative._
+import cats.Apply
+import cats.instances.future._
+
 object TraverseEx extends App {
   val hostnames = List(
     "www.google.com",
@@ -15,7 +21,21 @@ object TraverseEx extends App {
     Future(hostname.length * 60)
 
   val allUptimes: Future[List[Int]] = Future.traverse(hostnames)(getUptime)
+
   println(
     Await.result(allUptimes, 1.second)
+  )
+
+  // traverse list of applicatives
+  def listTraverse[F[_] : Applicative, A, B](list: List[A])(func: A => F[B]): F[List[B]] =
+    list.foldLeft(List.empty[B].pure[F]) { (accum, item) =>
+      Apply[F].map2(accum, func(item))(_ :+ _)
+    }
+
+  def listSequence[F[_] : Applicative, B](list: List[F[B]]): F[List[B]] =
+    listTraverse(list)(identity)
+
+  println(
+    Await.result(listTraverse(hostnames)(getUptime), 1.second)
   )
 }
