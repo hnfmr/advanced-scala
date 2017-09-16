25d59408b933e1f3ef7fbc313bc4dae03878a521
Wei Qi <qiwei.uk@gmail.com>
2017-09-16 20:57:50 +1000

    more traverse ex

--- src/main/scala/io/hnfmr/chapter7/TraverseEx.scala
+++ src/main/scala/io/hnfmr/chapter7/TraverseEx.scala
@@ -10,6 +10,10 @@ import cats.Applicative
 import cats.syntax.applicative._
 import cats.Apply
 import cats.instances.future._
+import cats.instances.vector._
+import cats.instances.option._
+import cats.data.Validated
+import cats.instances.list._
 
 object TraverseEx extends App {
   val hostnames = List(
@@ -37,5 +41,42 @@ object TraverseEx extends App {
 
   println(
     Await.result(listTraverse(hostnames)(getUptime), 1.second)
+  )
+
+  println(
+    listSequence(List(Vector(1,2), Vector(3,4)))
+  )
+
+  println(
+    listSequence(List(Vector(1,2), Vector(3,4), Vector(5,6)))
+  )
+
+  println(
+    process(List(2,4,6))
+  )
+
+  def process(inputs: List[Int]) =
+    listTraverse(inputs)(n => if (n % 2 == 0) Some(n) else None)
+
+  println(
+    process(List(1,2,3))
+  )
+
+  type ErrorsOr[A] = Validated[List[String], A]
+  def process2(inputs: List[Int]): ErrorsOr[List[Int]] =
+    listTraverse(inputs) { n =>
+      if (n % 2 == 0) {
+        Validated.valid(n)
+      } else {
+        Validated.invalid(List(s"$n is not even"))
+      }
+    }
+
+  println(
+    process2(List(2,4,6))
+  )
+
+  println(
+    process2(List(1,2,3))
   )
 }
