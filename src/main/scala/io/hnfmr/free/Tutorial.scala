2d920b290a82f255b36da707a77850ba57201386
Wei Qi <wei.qi@invoice2go.com>
2017-12-15 16:42:55 +1100

    Free monad tutorial

---
+++ src/main/scala/io/hnfmr/free/Tutorial.scala
@@ -0,0 +1,155 @@
+package io.hnfmr.free
+
+import cats.free.Free
+import cats.free.Free.liftF
+
+import cats.arrow.FunctionK
+import cats.{Id, ~>, InjectK}
+import scala.collection.mutable
+import scala.collection.mutable.ListBuffer
+
+import cats.data.State
+import cats.data.EitherK
+
+object Tutorial extends App {
+
+  sealed trait KVStoreA[A]
+  case class Put[T](key: String, value: T) extends KVStoreA[Unit]
+  case class Get[T](key: String) extends KVStoreA[Option[T]]
+  case class Delete(key: String) extends KVStoreA[Unit]
+
+  type KVStore[A] = Free[KVStoreA, A]
+
+  def put[T](key: String, value: T): KVStore[Unit] =
+    liftF[KVStoreA, Unit](Put[T](key, value))
+
+  def get[T](key: String): KVStore[Option[T]] =
+    liftF[KVStoreA, Option[T]](Get[T](key))
+
+  def delete(key: String): KVStore[Unit] =
+    liftF(Delete(key))
+
+  def update[T](key: String, f: T => T): KVStore[Unit] =
+    for {
+      vMaybe <- get[T](key)
+      _ <- vMaybe.map(v => put[T](key, f(v))).getOrElse(Free.pure(()))
+    } yield ()
+
+  // Impure Compiler
+  def impureCompiler: KVStoreA ~> Id =
+    new (KVStoreA ~> Id) {
+      // a very simple and (imprecise) key-value store
+      val kvs = mutable.Map.empty[String, Any]
+
+      def apply[A](fa: KVStoreA[A]): Id[A] =
+        fa match {
+          case Put(key, value) =>
+            println(s"put($key, $value)")
+            kvs(key) = value
+            ()
+          case Get(key) =>
+            println(s"get($key)")
+            kvs.get(key).map(_.asInstanceOf[A])
+          case Delete(key) =>
+            println(s"delete($key)")
+            kvs.remove(key)
+            ()
+        }
+    }
+
+  def program: KVStore[Option[Int]] =
+    for {
+      _ <- put("wild-cats", 2)
+      _ <- update[Int]("wild-cats", (_ + 12))
+      _ <- put("tame-cats", 5)
+      n <- get[Int]("wild-cats")
+      _ <- delete("tame-cats")
+    } yield n
+
+  val result: Option[Int] = program.foldMap(impureCompiler)
+  println(result)
+
+  // Pure compiler
+  type KVStoreState[A] = State[Map[String, Any], A]
+
+  val pureCompiler: KVStoreA ~> KVStoreState = new (KVStoreA ~> KVStoreState) {
+    def apply[A](fa: KVStoreA[A]): KVStoreState[A] =
+      fa match {
+        case Put(key, value) => State.modify(_.updated(key, value))
+        case Get(key) =>
+          State.inspect(_.get(key).map(_.asInstanceOf[A]))
+        case Delete(key) => State.modify(_ - key)
+      }
+  }
+
+  val result1: (Map[String, Any], Option[Int]) = program.foldMap(pureCompiler).run(Map.empty).value
+  println(result1)
+
+  //------Composing Free monads ADTs
+  // handles user interaction
+  sealed trait Interact[A]
+  case class Ask(prompt: String) extends Interact[String]
+  case class Tell(msg: String) extends Interact[Unit]
+
+  // represents persistence operations
+  sealed trait DataOp[A]
+  case class AddCat(a: String) extends DataOp[Unit]
+  case class GetAllCats() extends DataOp[List[String]]
+
+  type CatsApp[A] = EitherK[DataOp, Interact, A]
+
+  class Interacts[F[_]](implicit I: InjectK[Interact, F]) {
+    def tell(msg: String): Free[F, Unit] = Free.inject[Interact, F](Tell(msg))
+    def ask(prompt: String): Free[F, String] = Free.inject[Interact, F](Ask(prompt))
+  }
+
+  object Interacts {
+    implicit def interacts[F[_]](implicit I: InjectK[Interact, F]): Interacts[F] = new Interacts[F]
+  }
+
+  class DataSource[F[_]](implicit I: InjectK[DataOp, F]) {
+    def addCat(a: String): Free[F, Unit] = Free.inject[DataOp, F](AddCat(a))
+    def getAllCats: Free[F, List[String]] = Free.inject[DataOp, F](GetAllCats())
+  }
+
+  object DataSource {
+    implicit def dataSource[F[_]](implicit I: InjectK[DataOp, F]): DataSource[F] = new DataSource[F]
+  }
+
+  def program1(implicit I: Interacts[CatsApp], D: DataSource[CatsApp]): Free[CatsApp, Unit] = {
+    import I._, D._
+    for {
+      cat <- ask("What's the kitty's name?")
+      _ <- addCat(cat)
+      cats <- getAllCats
+      _ <- tell(cats.toString)
+    } yield ()
+  }
+
+  object ConsoleCatsInterpreter extends (Interact ~> Id) {
+    def apply[A](i: Interact[A]) = i match {
+      case Ask(prompt) =>
+        println(prompt)
+        scala.io.StdIn.readLine()
+      case Tell(msg) =>
+        println(msg)
+    }
+  }
+
+  object InMemoryDatasourceInterpreter extends (DataOp ~> Id) {
+
+    private[this] val memDataSet = new ListBuffer[String]
+
+    def apply[A](fa: DataOp[A]) = fa match {
+      case AddCat(a) => memDataSet.append(a); ()
+      case GetAllCats() => memDataSet.toList
+    }
+  }
+
+  val interpreter: CatsApp ~> Id = InMemoryDatasourceInterpreter or ConsoleCatsInterpreter
+  import DataSource._, Interacts._
+
+  val evaled: Unit = program1.foldMap(interpreter)
+  println("-"*50)
+  println(evaled)
+}
